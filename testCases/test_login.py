e1f19f148388e126e22c6e6d739428728d3f9837
Brian0213 <olusegunojeyinka@gmail.com>
2023-01-09 15:12:44 -0500

    Update

--- testCases/test_login.py
+++ testCases/test_login.py
@@ -27,7 +27,6 @@ class Test_001_Login:
         self.lp.clickLogin()
         self.logger.info("******** Login Test is successful *******")
         # self.driver.assert_text()
-        # self.driver.save_screenshot(self, r"C:\Users\oluse\PycharmProjects\ProjectTest\Screenshots\HomePage.png")
         self.driver.save_screenshot("/Users/oluse/PycharmProjects/ProjectTest/Screenshots/LoginPage.png")
         self.driver.close()
         self.driver.quit()
