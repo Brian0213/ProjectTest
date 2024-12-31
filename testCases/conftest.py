4fb9d6cfe092b3ad23531461a582039cbf64a581
Brian0213 <91477623+Brian0213@users.noreply.github.com>
2024-12-30 23:29:01 -0500

    Update conftest.py

--- testCases/conftest.py
+++ testCases/conftest.py
@@ -17,8 +17,9 @@ chrome_options = Options()
 
 options = webdriver.ChromeOptions()
 options.add_argument("start-maximized")
-# options.add_argument("headless")
+options.add_argument("headless")
 options.add_argument("--disable-dev-shm-usage")
+options.add_argument("--no-sandbox")
 options.add_experimental_option('excludeSwitches', ['enable-logging'])
 
 
